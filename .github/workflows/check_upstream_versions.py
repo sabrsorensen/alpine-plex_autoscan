42320a2c40db9103d5bdd382c74d6ca050aefbe4
Sam Sorensen <825813+sabrsorensen@users.noreply.github.com>
2020-08-23 23:54:25 -0600

    Added image version checking.

--- .github/workflows/check_upstream_versions.py
+++ .github/workflows/check_upstream_versions.py
@@ -8,6 +8,7 @@ import json
 import os
 import requests
 import sys
+from git import Repo
 
 old_versions = {}
 current_versions = {}
@@ -18,6 +19,8 @@ try:
         os.environ['GITHUB_REPOSITORY'], command='rclone version', auto_remove=True, entrypoint='').decode('UTF-8').split('\n')[0].split(' ')[1].strip()
     old_versions['plex_autoscan_commit_ref'] = client.containers.run(
         os.environ['GITHUB_REPOSITORY'], command='git --work-tree=/opt/plex_autoscan rev-parse HEAD', auto_remove=True, entrypoint='').decode('UTF-8').strip()
+    old_versions['alpine-plex_autoscan_commit_ref'] = client.images.get(
+        os.environ['GITHUB_REPOSITORY']).labels['org.label-schema.vcs-ref']
     print("Detected image versions:\n" + json.dumps(old_versions, indent=2))
 except docker.errors.ContainerError as e:
     print(e)
@@ -26,12 +29,14 @@ current_versions['rclone_release_name'] = requests.get(
     'https://api.github.com/repos/rclone/rclone/releases/latest').json()["tag_name"]
 current_versions['plex_autoscan_commit_ref'] = requests.get(
     'https://api.github.com/repos/l3uddz/plex_autoscan/commits/develop').json()["sha"]
+current_versions['alpine-plex_autoscan_commit_ref'] = Repo(
+    '.').commit('HEAD').hexsha[:7]
 
 print("Current detected versions:\n" + json.dumps(current_versions, indent=2))
 
 if old_versions == current_versions:
     print("No version change, skipping image rebuild.")
-    print("::set-env name=REBUILD::true")
+    print("::set-env name=REBUILD::false")
 else:
     print("Upstream versions changed.")
     print("::set-env name=REBUILD::true")
