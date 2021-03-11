00eac031e74dd961242549451e135982fe0f1f66
Sam Sorensen <825813+sabrsorensen@users.noreply.github.com>
2021-03-11 12:05:33 -0700

    Fixed rclone version cehck.

--- .github/workflows/check_upstream_versions.sh
+++ .github/workflows/check_upstream_versions.sh
@@ -2,11 +2,13 @@
 
 image_plex_autoscan_ref=`docker run $GITHUB_REPOSITORY "git --work-tree=/opt/plex_autoscan rev-parse HEAD"`
 image_s6_release=`docker run $GITHUB_REPOSITORY "cat /etc/S6_RELEASE"`
+image_rclone_version=`docker run $GITHUB_REPOSITORY "rclone version" | grep rclone | awk '{print $2}'`
 
 current_plex_autoscan_ref=`curl -sX GET "https://api.github.com/repos/l3uddz/plex_autoscan/commits/develop" | jq '.sha' | tr -d '"'`
 current_s6_release=`curl -sX GET "https://api.github.com/repos/just-containers/s6-overlay/releases/latest" | jq '.tag_name' | tr -d '"'`
+current_rclone_version=`docker run rclone/rclone "version" | grep rclone | awk '{print $2}'`
 
-if [ `docker run $GITHUB_REPOSITORY "rclone version --check" | grep -P "yours|latest" | awk '{print $2}' | uniq | wc -l` == 2 ]
+if [ $image_rclone_version != $current_rclone_version ]
 then
     echo "New version of rclone is available."
     build=1
